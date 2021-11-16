b3088599f808da1f9bde0f5f3a70434543cfb4f4
Nicholas Martens <darkdragon.martens@gmail.com>
2021-11-16 15:51:05 +0100

    Save before push

--- src/routes/auth/login.svelte
+++ src/routes/auth/login.svelte
@@ -6,26 +6,26 @@
 <div class="flex justify-center pt-10">
   <div class="w-1/3 bg-white rounded-xl">
     <form class="p-6">
-      <h1 class="text-3xl text-center pb-5">Join game</h1>
+      <h1 class="text-3xl text-center pb-5">Login</h1>
 
-      <label for="gameId" class="pb-1 block">Game ID</label>
+      <label for="gameId" class="pb-1 block">Username</label>
       <Input
         type="text"
         id="gameId"
-        placeholder="Enter your game ID"
+        placeholder="Enter your username"
         classes="mb-3"
       />
 
-      <label for="gamePassword" class="pb-1 block">Game Password</label>
+      <label for="gamePassword" class="pb-1 block">Password</label>
       <Input
         type="text"
         id="gamePassword"
-        placeholder="Enter the game password"
+        placeholder="Enter your password assword"
         classes="mb-3"
       />
 
       <Button
-        text="Join game"
+        text="Login"
         gradientStart="green-500"
         gradientEnd="green-600"
         classes="w-full mt-3 mb-1"
